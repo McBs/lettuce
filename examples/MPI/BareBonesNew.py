a19250111882aea81600fc07f71fd15762c7bf1f
Michael Brilka <33370237+Michael-Brilka@users.noreply.github.com>
2021-10-01 15:05:13 +0200

    Update BareBonesNew.py

--- examples/MPI/BareBonesNew.py
+++ examples/MPI/BareBonesNew.py
@@ -7,28 +7,25 @@ def run(device, mpiObject):
     print("start")
 
     # ---------- Set up simulation -------------
-    #device = torch.device("cuda:0")  # replace with device("cpu"), if no GPU is available
-    lattice = lt.Lattice(lt.D2Q9, device=device, dtype=torch.float64)  # single precision - float64 for double precision
+    lattice = lt.Lattice(lt.D2Q9, device=device, dtype=torch.float64,mpiObject=mpiObject)  # single precision - float64 for double precision
     resolution = 120  # resolution of the lattice, low resolution leads to unstable speeds somewhen after 10 (PU)
-    flow = lt.TaylorGreenVortex2D(resolution=resolution, reynolds_number=10000, mach_number=0.05, lattice=lattice,mpiObject=mpiObject)
+    flow = lt.TaylorGreenVortex2D(resolution=resolution, reynolds_number=10000, mach_number=0.05, lattice=lattice)
     print(flow.units.relaxation_parameter_lu)
     # select collision model - try also KBCCollision or RegularizedCollision
     collision = lt.BGKCollision(lattice, tau=flow.units.relaxation_parameter_lu)
-    streaming = lt.StandardStreaming(lattice,mpiObject=mpiObject)
-    simulation = lt.Simulation(flow, lattice, collision, streaming,mpiObject=mpiObject)
-    #VTKreport = lt.VTKReporter(lattice, flow, interval=100,filename_base="./data/GRE/Eoutput",mpiObj=mpiObject)
-    err=lt.ErrorReporter(lattice,flow,interval=1)
+    streaming = lt.StandardStreaming(lattice)
+    simulation = lt.Simulation(flow, lattice, collision, streaming)
+    VTKreport = lt.VTKReporter(lattice, flow, interval=100,filename_base="./data/GRE/Eoutput")
+   
     #simulation.initialize_f_neq()  # initialize non-conserved moments
-    simulation.reporters.append(err)
-    # reporters will grab the results in between simulation steps (see io.py and simulation.py)
-    # Output: Column 1: time in LU, Column 2: kinetic energy in PU
+    simulation.reporters.append(VTKreport)
+   
    
     # ---------- Simulate until time = 10 (PU) -------------
     print("Simulating", int(simulation.flow.units.convert_time_to_lu(10)), "steps! Maybe drink some water in the meantime.")
     # runs simulation, but also returns overall performance in MLUPS (million lattice units per second)
 
     x=simulation.flow.units.convert_time_to_lu(10)
-    x=10
     print("MLUPS: ", simulation.step(int(x)))
     
     print("Python end")
@@ -40,6 +37,6 @@ if __name__ == "__main__":
     device = torch.device("cpu")
     pcList=[[0,20],[1,40]]
     gpuList=[[4,"gpu-node001"],[4,"gpu-node002"],[4,"gpu-node003"],[4,"gpu-node004"],[1,"gpu-node005"],[1,"gpu-node006"],[1,"gpu-node007"],[1,"gpu-node008"],[2,"gpu-node009"],[2,"gpu-node010"]]
-    mpiOBJ=lt.mpiObject(0,sizeList=pcList,gpuList=gpuList,setParts=0,gridRefinment=0)
+    mpiOBJ=lt.mpiObject(0,sizeList=pcList,gpuList=gpuList,setParts=0,gridRefinment=0,printUserInfo=1)
     lt.running(run,device,mpiOBJ)
     
