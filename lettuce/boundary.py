d026fb320842a1a067028e182019b5c39c658e47
Ben Picard <ben.picard@web.de>
2023-11-29 16:08:30 +0100

    Umsortieren udn verschönern

--- lettuce/boundary.py
+++ lettuce/boundary.py
@@ -368,39 +368,51 @@ class newsuperTGV3D:
                                              ,[(slice(None),-1,slice(None)),(0,slice(None),slice(None))]]
 
         self.borders=[(0,-1,slice(None)),(-1,0,slice(None)),(0,0,slice(None)),(-1,-1,slice(None)),(slice(None),-1,0)
-                                  ,(-1,slice(None),0),(0,slice(None),0),(slice(None),0,0),(slice(None),0,-1),(slice(None),-1,-1),
+                                  ,(-1,slice(None),0),(0,slice(None),0),(slice(None),0,0)
+            ,(slice(None),0,-1),(slice(None),-1,-1),
             (slice(None),0,0),(-1,slice(None),0)]
         self.corners=[(1,1,1),(-1,-1,-1),(1,1,-1),(-1,-1,1),(1,-1,1),(-1,1,-1),(1,-1,-1),(-1,1,1)]
 
     def __call__(self, f):
 
-        self.f_copies=torch.stack((f[:,0,:,:].clone(),f[:,:,0,:].clone(),f[:,:,:,-1].clone(),f[:,:,:,0].clone(),f[:,-1,:,:].clone(),f[:,:,-1,:].clone()), dim=3)
+        self.f_copies=torch.stack((f[:,0,:,:].clone(),f[:,:,0,:].clone(),f[:,:,:,-1].clone(),
+                                   f[:,:,:,0].clone(),f[:,-1,:,:].clone(),f[:,:,-1,:].clone()), dim=3)
 
-        self.f_copies_borders=torch.stack((f[:,0,-1,:].clone(),f[:,-1,0,:].clone(),f[:,-1,-1,:].clone(),f[:,0,0,:].clone(),f[:,:,0,-1].clone()
-                                           ,f[:,0,:,-1].clone(),f[:,:,-1,-1].clone(),f[:,-1,:,-1].clone(),f[:,:,-1,0].clone(),f[:,0,:,0].clone()
-                                           ,f[:,:,0,0].clone(),f[:,-1,:,0].clone()),dim=2)
+        self.f_copies_borders=torch.stack((f[:,0,-1,:].clone(),f[:,-1,0,:].clone(),f[:,-1,-1,:].clone(),
+                                            f[:,0,0,:].clone(),f[:,:,0,-1].clone()
+                                            ,f[:,0,:,-1].clone(),f[:,:,-1,-1].clone(),
+                                            f[:,-1,:,-1].clone(),f[:,:,-1,0].clone(),
+                                            f[:,0,:,0].clone()
+                                            ,f[:,:,0,0].clone(),f[:,-1,:,0].clone()),dim=2)
 
-        self.f_copies_corners = torch.stack([f[:, 1, 1, 1].clone(), f[:,-1,-1,-1].clone(), f[:,1,1,-1].clone(), f[:,-1,-1,1].clone(),
-                                 f[:,1,-1,1].clone(),f[:,-1,1,-1].clone(),f[:, 1,-1,-1].clone(),f[:, -1,1,1].clone()],dim=1)
+        self.f_copies_corners = torch.stack([f[:, 1, 1, 1].clone(), f[:,-1,-1,-1].clone(),
+                                             f[:,1,1,-1].clone(), f[:,-1,-1,1].clone(),
+                                             f[:,1,-1,1].clone(),f[:,-1,1,-1].clone(),
+                                             f[:, 1,-1,-1].clone(),f[:, -1,1,1].clone()],dim=1)
 
         for i in range(6):
             for j in range(len(self.switch_stencil_wall[i])):
 
                 if i == 3:
-                    f[self.switch_stencil_wall[i][j][1],*self.swap_w[i][1]] = torch.transpose(self.f_copies[self.switch_stencil_wall[i][j][0],:, :, i],0,1)
+                    f[self.switch_stencil_wall[i][j][1],*self.swap_w[i][1]] = \
+                        torch.transpose(self.f_copies[self.switch_stencil_wall[i][j][0],:, :, i],0,1)
                 else:
 
-                    f[self.switch_stencil_wall[i][j][1],*self.swap_w[i][1]]=self.f_copies[self.switch_stencil_wall[i][j][0],:,:,i]
+                    f[self.switch_stencil_wall[i][j][1],*self.swap_w[i][1]]=\
+                        self.f_copies[self.switch_stencil_wall[i][j][0],:,:,i]
 
 
         for i in range(12):
             for j in range(len(self.switch_stencil_borders[i])):
-                f[self.switch_stencil_borders[i][j][0], *self.borders[i]] = self.f_copies_borders[self.switch_stencil_borders[i][j][1],:, i]
+                f[self.switch_stencil_borders[i][j][0], *self.borders[i]] = \
+                    self.f_copies_borders[self.switch_stencil_borders[i][j][1],:, i]
 
         if any(inner for inner in self.switch_stencil_corner):
             for i in range(4):
 
-                f[self.switch_stencil_corner[0][2*i][0],*self.corners[2*i]]=self.f_copies_corners[self.switch_stencil_corner[0][2*i][1],2*i+1]
-                f[self.switch_stencil_corner[0][2*i+1][0],*self.corners[2*i+1]]=self.f_copies_corners[self.switch_stencil_corner[0][2*i+1][1],2*i]
+                f[self.switch_stencil_corner[0][2*i][0],*self.corners[2*i]]=\
+                    self.f_copies_corners[self.switch_stencil_corner[0][2*i][1],2*i+1]
+                f[self.switch_stencil_corner[0][2*i+1][0],*self.corners[2*i+1]]=\
+                    self.f_copies_corners[self.switch_stencil_corner[0][2*i+1][1],2*i]
 
         return f
