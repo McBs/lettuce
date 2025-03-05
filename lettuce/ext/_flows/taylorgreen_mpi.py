3e3c209c20aafcec1ea06fc78c681dbb5431666c
random36 <github@becker-sec.de>
2025-03-05 12:35:25 +0100

    testing

--- lettuce/ext/_flows/taylorgreen_mpi.py
+++ lettuce/ext/_flows/taylorgreen_mpi.py
@@ -58,25 +58,29 @@ class TaylorGreenVortex_mpi(ExtFlow):
         print(dist.get_rank())
         print(self.resolution[0])
         nodes = int(self.resolution[0] / dist.get_world_size())
+        print("--------nodes----------")
         print(nodes)
         endpoints = [2 * torch.pi * (1 - 1 / n) for n in
                      self.resolution]  # like endpoint=False in np.linspace
+        print("--------nodes----------")
         print(endpoints)
         for n in range(self.stencil.d): 
             lspace=torch.linspace(0, endpoints[n],
                                        steps=self.resolution[n],
                                        device=self.context.device,
                                        dtype=self.context.dtype)
-        print("lspace:" + lspace)
-
-        print("split lspace" + torch.split(lspace, nodes, dim=0))
+        print("--------lspace----------")
+        print(lspace)
+        print("--------split lspace----------")
+        print(torch.split(lspace, nodes, dim=0))
 
         xyz_test = tuple(torch.split(torch.linspace(0, endpoints[n],
                                    steps=self.resolution[n],
                                    device=self.context.device,
                                    dtype=self.context.dtype), nodes, dim=0)
                     for n in range(self.stencil.d))
-        print("xyz:" + str(xyz_test))
+        print("--------xyz with split lspace----------")
+        print(xyz_test)
 
 
         xyz = tuple(torch.linspace(0, endpoints[n],
@@ -84,7 +88,8 @@ class TaylorGreenVortex_mpi(ExtFlow):
                                    device=self.context.device,
                                    dtype=self.context.dtype)
                     for n in range(self.stencil.d))
-        print("xyz:" + str(xyz))
+        print("--------xyz----------")
+        print(xyz)
         return torch.meshgrid(*xyz, indexing='ij')
 
     def initial_pu(self) -> (torch.Tensor, torch.Tensor):
