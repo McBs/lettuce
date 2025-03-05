b22acba7ba88127a3f63eb2f8e19cfc0fa564433
random36 <github@becker-sec.de>
2025-03-05 16:03:16 +0100

    testing

--- lettuce/ext/_flows/taylorgreen_mpi.py
+++ lettuce/ext/_flows/taylorgreen_mpi.py
@@ -73,7 +73,8 @@ class TaylorGreenVortex_mpi(ExtFlow):
                                        steps=self.resolution[0],
                                        device=self.context.device,
                                        dtype=self.context.dtype), nodes, dim=0)
-        for n in range(self.stencil.d - 1): 
+        
+        for n in range(1, self.stencil.d): 
             lspace=torch.linspace(0, endpoints[n],
                                        steps=self.resolution[n],
                                        device=self.context.device,
