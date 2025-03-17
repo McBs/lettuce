ed30e9d23b74f4c34135b83981019aee76579599
random36 <github@becker-sec.de>
2025-03-17 22:10:14 +0100

    test if grid can be split with torch.cat

--- lettuce/ext/_flows/taylorgreen.py
+++ lettuce/ext/_flows/taylorgreen.py
@@ -87,13 +87,13 @@ class TaylorGreenVortex(ExtFlow):
             print("-----splits-----")
             print(splits[1])
 
-            #extended_splits = []
-            #for i in range(dist.get_world_size()):
-            #    left_neighbor = splits[i-1][-1:] if i > 0 else splits[-1][-1:]  
-            #    right_neighbor = splits[i+1][:1] if i < dist.get_world_size() - 1 else splits[0][:1] 
+            extended_splits = []
+            for i in range(dist.get_world_size()):
+                left_neighbor = splits[i-1][-1:] if i > 0 else splits[-1][-1:]  
+                right_neighbor = splits[i+1][:1] if i < dist.get_world_size() - 1 else splits[0][:1] 
 
-            #    extended_split = torch.cat([left_neighbor, splits[i], right_neighbor])
-            #    extended_splits.append(extended_split)
+                extended_split = torch.cat([left_neighbor, splits[i], right_neighbor])
+                extended_splits.append(extended_split)
 
             xyz = tuple((splits[dist.get_rank()],
                         torch.linspace(0, endpoints[n],
