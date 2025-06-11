4e744cfa76a03f64f4de98d89e5771f42f5bb5a3
random36 <github@becker-sec.de>
2025-06-11 20:45:18 +0200

    testing

--- lettuce/_simulation.py
+++ lettuce/_simulation.py
@@ -263,11 +263,11 @@ class Simulation:
             print(dist)
             if self.dist == "mpi":
                 filename = "/home/mbecke3g/data/" + str(self.flow.i) + "_rank_" + str(dist.get_rank()) + ".pt"
-                print("Rank: " + str(dist.get_rank()) + " World Size: " + str(dist.get_world_size()) + " Flow.f shape: " + str(self.flow.i.shape))
+                print("Rank: " + str(dist.get_rank()) + " World Size: " + str(dist.get_world_size()) + " Flow.f shape: " + str(self.flow.f.shape))
                 torch.save(self.flow.f, filename)
             else:
                 filename = "/home/mbecke3g/data/" + str(self.flow.i) + "serial" + ".pt"
-                print("Serial:  Flow.f shape: " + str(self.flow.i.shape))
+                print("Serial:  Flow.f shape: " + str(self.flow.f.shape))
                 torch.save(self.flow.f, filename)
             self.flow.i += 1
             self._report()
