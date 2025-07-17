8fd623a7c50736a0f1e76883f1a3d80bc432ce15
random36 <github@becker-sec.de>
2025-07-17 12:31:11 +0200

    fixing axis

--- lettuce/_flow.py
+++ lettuce/_flow.py
@@ -106,36 +106,12 @@ class Flow(ABC):
         ...
 
     def initialize(self):
-        #exportire initial_rho, initial_u für ranks ausgeben und vergleichen.
         print("--------------------Flow initalize---------")
         """initializing in equilibrium"""
         initial_p, initial_u = self.initial_pu()
-        if self.dist == "mpi":
-            filename = "/home/mbecke3g/data/initial_p_" + str(dist.get_rank()) + ".pt"
-            print("Rank: " + str(dist.get_rank()) + " World Size: " + str(dist.get_world_size()) + " initial_p shape: " + str(initial_p.shape))
-            torch.save(initial_p, filename)
-        else:
-            filename = "/home/mbecke3g/data/initial_p_serial" + ".pt"
-            print("Serial:  initial_p shape: " + str(initial_p.shape))
-            torch.save(initial_p, filename)
-        if self.dist == "mpi":
-            filename = "/home/mbecke3g/data/initial_u_" + str(dist.get_rank()) + ".pt"
-            print("Rank: " + str(dist.get_rank()) + " World Size: " + str(dist.get_world_size()) + " initial_u shape: " + str(initial_u.shape))
-            torch.save(initial_u, filename)
-        else:
-            filename = "/home/mbecke3g/data/initial_u_serial" + ".pt"
-            print("Serial:  initial_u shape: " + str(initial_u.shape))
-            torch.save(initial_u, filename)    
+        
         initial_rho = self.context.convert_to_tensor(
             self.units.convert_pressure_pu_to_density_lu(initial_p))
-        if self.dist == "mpi":
-            filename = "/home/mbecke3g/data/initial_rho_" + str(dist.get_rank()) + ".pt"
-            print("Rank: " + str(dist.get_rank()) + " World Size: " + str(dist.get_world_size()) + " initial_rho shape: " + str(initial_rho.shape))
-            torch.save(initial_rho, filename)
-        else:
-            filename = "/home/mbecke3g/data/initial_rho_serial" + ".pt"
-            print("Serial:  initial_rho shape: " + str(initial_rho.shape))
-            torch.save(initial_rho, filename)
         initial_u = self.context.convert_to_tensor(
             self.units.convert_velocity_to_lu(initial_u))
         if self.initialize_pressure:
@@ -146,7 +122,6 @@ class Flow(ABC):
             )
             self.f = self.equilibrium(self, rho=initial_rho, u=initial_u)
         self.f = self.equilibrium(self, rho=initial_rho, u=initial_u)
-        # set initialize_f_neq = flase 
         if self.initialize_fneq:
             self.f = initialize_f_neq(self)
 
